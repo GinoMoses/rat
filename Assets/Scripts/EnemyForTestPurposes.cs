4658026fe886493cbff7ab0ee9a3e8ee119fb890
Damian Gorol <damian.gorol78@gmail.com>
2024-04-24 22:12:29 +0200

    YESSS SHIT

--- Assets/Scripts/EnemyForTestPurposes.cs
+++ Assets/Scripts/EnemyForTestPurposes.cs
@@ -6,23 +6,22 @@ public class EnemyTest : MonoBehaviour
 {
     public float maxHealth = 100;
     public float currentHealth;
-
-    [SerializeField] FloatingHealthbar healthbar;
+    public EnemyHealthCount healthcount;
 
     private void Awake() {
-        healthbar = GetComponentInChildren<FloatingHealthbar>();
+        healthcount = GetComponentInChildren<EnemyHealthCount>();
     }
     void Start()
     {
         currentHealth = maxHealth;
-        healthbar.UpdateHealthBar(currentHealth, maxHealth);
+        healthcount.UpdateHealthCount(currentHealth, maxHealth);
     }
 
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
-        healthbar.UpdateHealthBar(currentHealth, maxHealth);
+        healthcount.UpdateHealthCount(currentHealth, maxHealth);
         if(currentHealth <= 0)
         {
             // I ain't coding a death for a test enemy lmao
